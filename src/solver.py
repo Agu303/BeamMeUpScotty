42e2562e73c17465501181c1b03b4bb1ffbe573d
Al_Gue <63983052+Agu303@users.noreply.github.com>
2025-04-17 11:36:27 -0500

    Create solver.py

---
+++ src/solver.py
@@ -0,0 +1,35 @@
+class BeamSolver:
+   
+   '''
+    def __init__(self):
+
+    def ini_Load:
+
+    def inp_UDL: #uniform distributed load
+
+    def inp_PL: #point load
+
+    def inp_PUDL: #partial uniform distributed load
+
+    def inp_MomL: #moment load
+    
+    def inp_LVL: #linear varying load (triangle, trapezoidal)
+    
+    def inp_AL: #axial load
+
+    def Beam_solve: #connected to results.py beamResult class
+    
+    def forces: 
+    
+    def glob_stiff:
+    
+    def bc:
+    
+    def reactions: #calc reatcions
+    
+    def displacment: #solves stiffness matrix w/ force vectors
+        
+    def plotting: #SF, BM, Deflection, Distance along beam
+        ...
+        plt.show()
+    '''
